d8db3cb35ba36473064c3816b151861760fe0e2c
Tran Manh Linh <devngmo@gmail.com>
2022-05-20 16:29:01 +0700

    + add UIManager

---
+++ ui/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace tmlpaks.commons.ui
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        private Dictionary<string, RectTransform> dialogMap = new Dictionary<string, RectTransform>();
+
+        public void addDialog(string dialogID, RectTransform dialogTransform)
+        {
+            if (dialogMap.ContainsKey(dialogID))
+                dialogMap[dialogID] = dialogTransform;
+            else
+                dialogMap.Add(dialogID, dialogTransform);
+        }
+        public void showDialog(string dialogID)
+        {
+            dialogMap[dialogID].gameObject.SetActive(true);
+        }
+
+
+    }
+}
